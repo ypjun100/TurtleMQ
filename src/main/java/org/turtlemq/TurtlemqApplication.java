98fa0d0f75ea44b68643fe77a2dcce1ddde04286
ypjun100 <ypjun100@gmail.com>
2023-10-20 22:50:37 +0900

    ADD: Create base project

---
+++ src/main/java/org/turtlemq/TurtlemqApplication.java
@@ -0,0 +1,13 @@
+package org.turtlemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtlemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtlemqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/turtlemq/TurtlemqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turtlemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtlemqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
